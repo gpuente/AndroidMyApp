108012591142abc24d751583d2b5146cbb567a33
Guillermo Puente <gpuente@borealis.cl>
2017-03-23 17:12:45 -0300

    first commit

---
+++ app/src/main/java/com/guillermopuente/myapp/model/Alumno.java
@@ -0,0 +1,38 @@
+package com.guillermopuente.myapp.model;
+
+import java.util.Date;
+
+/**
+ * Created by Guillermo Puente on 23-03-2017.
+ */
+
+public class Alumno {
+
+    private String nombre;
+    private long numeroCuenta;
+    private Date fechaNacimiento;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(long numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
